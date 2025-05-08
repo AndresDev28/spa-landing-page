636710cbdac0d65f09f08e414c4699fb538fe2a2
AndresDev28 <andresdev28@hotmail.com>
2025-05-08 15:39:40 +0200

    first commit

--- src/components/BestBusiness/BestBusiness.tsx
+++ src/components/BestBusiness/BestBusiness.tsx
@@ -33,7 +33,7 @@ export function BestBusiness() {
                 </div>
 
                 <div className="grid items-center py-5 md:p-8">
-                    {dataFeaturesBusiness.map(({ id, icon, title, description }) => (
+                    {dataFeaturesBusiness.map(({id, icon, title, description}) => (
                         <Reveal key={id}>
                             <div className="grid grid-flow-col gap-5 px-4 py-2 rounded-3xl group hover:bg-radialBlack">
                                 <Image src={`/assets/${icon}.png`} alt={title} width={40} height={40} />
--- src/components/PaymentsMethods/PaymentsMethods.tsx
+++ src/components/PaymentsMethods/PaymentsMethods.tsx
@@ -6,37 +6,37 @@ import { dataPaymentMethods } from './PaymentMethods.data'
 import Image from 'next/image'
 import { CtaDark } from '../CtaDark'
 
+
 export function PaymentsMethods() {
     return (
-        <div className="relative py-20 md:py-64" id="servicios">
-            <BackgroundRadialRight />
-            <div className="relative w-full overflow-hidden">
-                <Swiper
-                    breakpoints={{
-                        320: {
-                            slidesPerView: 8,
-                            spaceBetween: 15
-                        }
-                    }}
-                    autoplay={{
-                        delay: 1,
-                        disableOnInteraction: false
-                    }}
-                    grabCursor={true}
-                    loop={true}
-                    speed={2000}
-                >
-                    <div className="absolute flex">
-                        {dataPaymentMethods.map(({ id, image }) => (
-                            <SwiperSlide key={id} className="flex items-center slider-horizontal">
-                                <Image src={`/assets/${image}`} alt="Payment" width="70" height="70" className="h-[60px] w-[120px] object-contain" />
-                            </SwiperSlide>
-                        ))}
-                    </div>
-                </Swiper>
-            </div>
-
-            <CtaDark />
+        <div className='relative py-20 md:py-64' id='servicios'>
+          <BackgroundRadialRight />
+          <div className='relative w-full overflow-hidden'>
+            <Swiper
+              breakpoints={{
+                320: {
+                  slidesPerView: 8,
+                  spaceBetween: 15
+                }
+              }}
+              autoplay={{
+                delay: 1,
+                disableOnInteraction: false
+              }}
+              grabCursor={true}
+              loop={true}
+              speed={2000}
+            >
+              <div className='absolute flex'>
+                {dataPaymentMethods.map(({id, image}) => (
+                  <SwiperSlide key={id} className='flex items-center slider-horizontal'>
+                    <Image src={`/assets/${image}`} alt='Payment' width={70} height={70} className='h-[60px] w-[120px] objet-contain'/>
+                  </SwiperSlide>
+                ))}
+              </div>
+            </Swiper>
+          </div>
+          <CtaDark/>
         </div>
     )
 }
